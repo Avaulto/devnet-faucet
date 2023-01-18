dc5bf3a30b921996e589a0795d3a732061c32a23
amir cohen <inbox@avaulto.com>
2023-01-18 17:50:22 +0200

    add loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { Alert, Box, Container, Snackbar } from '@mui/material';
+import { Alert, Box, CircularProgress, Container, Snackbar } from '@mui/material';
 
 import { clusterApiUrl, Connection } from '@solana/web3.js';
-import {  FC, useState } from 'react';
+import { FC, useState } from 'react';
 import BtnFaucet from './components/btn-faucet/btn-faucet';
 import WalletInput from './components/wallet-input/wallet-input';
 import logo from './assets/logo_white.png';
+import Plausible from 'plausible-tracker';
 require('./App.css');
-
+const { trackPageview, enableAutoPageviews } = Plausible({
+    hashMode: true,
+})
+trackPageview()
+enableAutoPageviews()
 const App: FC = () => {
     const [address, setAddress] = useState<any>("");
     const connection = new Connection(clusterApiUrl('devnet'));
 
-    const [alertConfig, setAlertConfig] = useState({open: false, message: null});
+    const [alertConfig, setAlertConfig] = useState({ open: false, message: null });
     const [isValid, setIsValid] = useState<boolean | any>();
 
     return (
@@ -21,14 +26,14 @@ const App: FC = () => {
                 <h1>SOL ALL-IN Devnet Faucet</h1>
                 <WalletInput address={address} setAddress={setAddress} setIsValid={setIsValid} />
                 {isValid == false && <Alert severity="error">Invalid address</Alert>}
-                <BtnFaucet walletAddress={address} isValid={isValid} connection={connection} setAlertConfig={setAlertConfig}/>
-                <Snackbar open={alertConfig.open} autoHideDuration={6000} onClose={() => setAlertConfig({open: alertConfig.open, message: null})}>
+                <BtnFaucet walletAddress={address} isValid={isValid} connection={connection} setAlertConfig={setAlertConfig} />
+                <Snackbar open={alertConfig.open} autoHideDuration={6000} onClose={() => setAlertConfig({ open: false, message: null })}>
                     <Alert severity="success" sx={{ width: '100%' }}>
                         {alertConfig.message}
                     </Alert>
                 </Snackbar>
-            <span id='love'>built with LOVE by <a href="https://avaulto.com" target="_blank"><img src={logo} alt='avaulto logo'/></a></span><br/>
             </Container>
+            <span id='love'>built with LOVE by <a href="https://avaulto.com" target="_blank"><img src={logo} alt='avaulto logo' /></a></span><br />
 
         </div>
     );
--- src/components/btn-faucet/btn-faucet.tsx
+++ src/components/btn-faucet/btn-faucet.tsx
@@ -1,8 +1,8 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { keypairIdentity, Metaplex } from "@metaplex-foundation/js";
-import { BlockheightBasedTransactionConfirmationStrategy, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction, TransactionBlockhashCtor } from '@solana/web3.js';
-import { Box, Button } from '@mui/material';
-import { MINT_SIZE, TOKEN_PROGRAM_ID, getMinimumBalanceForRentExemptMint, createInitializeMintInstruction, createMint } from '@solana/spl-token';
+import { BlockheightBasedTransactionConfirmationStrategy, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction, TransactionBlockhashCtor, TransactionInstruction } from '@solana/web3.js';
+import { Box, Button, CircularProgress } from '@mui/material';
+import { MINT_SIZE, TOKEN_PROGRAM_ID, createTransferCheckedInstruction, createInitializeMintInstruction, createMint, getOrCreateAssociatedTokenAccount, createTransferInstruction, getAssociatedTokenAddress, createInitializeAccountInstruction, getMinimumBalanceForRentExemptAccount, ACCOUNT_SIZE, getMinimumBalanceForRentExemptMint, createAssociatedTokenAccountInstruction } from '@solana/spl-token';
 
 import Plausible from 'plausible-tracker'
 const { trackEvent } = Plausible();
@@ -14,6 +14,11 @@ interface BtnFaucetProps {
 }
 
 const BtnFaucet: FC<BtnFaucetProps> = ({ walletAddress, connection, setAlertConfig, isValid }) => {
+    const [loader, setLoader] = useState(false)
+    useEffect(() => {
+
+        console.log('rendered')
+    })
 
     const confirmAndFinalize = async (signature: string) => {
         const { lastValidBlockHeight, blockhash } = await connection.getLatestBlockhash();
@@ -31,76 +36,158 @@ const BtnFaucet: FC<BtnFaucetProps> = ({ walletAddress, connection, setAlertConf
             );
 
             await confirmAndFinalize(signature);
-            setAlertConfig({ open: true, message: 'Airdrop sol to account' });
-        } catch (err: any) {
-            console.error(err)
-
+        } catch (error) {
+            setLoader(false);
+            console.error(error)
         }
     }
-    const handleAirdrop = () => {
-        trackEvent('airdrop sol') 
-        requestAirdrop(walletAddress)
+    const handleAirdrop = async () => {
+        setLoader(true)
+        trackEvent('airdrop sol')
+        await requestAirdrop(walletAddress)
+        setAlertConfig({ open: true, message: 'Airdrop sol to account' });
+
+        setLoader(false)
+
     }
     const createDemoAccount = () => {
         const keypair = Keypair.generate();
         return keypair;
     };
     const requestNft = async () => {
-        trackEvent('airdrop nft') 
-        // create middleware account
-        const feePayer = createDemoAccount()
-        // airdrop this account
-        await requestAirdrop(feePayer.publicKey.toBase58())
+        try {
+            setLoader(true)
+            trackEvent('airdrop nft')
+            // create middleware account
+            const feePayer = createDemoAccount()
+            // airdrop this account
+            setAlertConfig({ open: true, message: 'Airdrop sol to fee payer account' });
+            await requestAirdrop(feePayer.publicKey.toBase58())
+            setAlertConfig({ open: true, message: 'Creating NFT' });
 
-        // init metaplex instance
-        const mx = new Metaplex(connection);
-        mx.use(keypairIdentity(feePayer));
-        const { nft } = await mx
-            .nfts()
-            .create({
-                tokenOwner: new PublicKey(walletAddress),
-                isCollection: true,
-                uri: "https://yyuf64d3dxl7pzwpyvwb24vqgztrxci5w3rvubbogt5s2d2m3weq.arweave.net/xihfcHsd1_fmz8VsHXKwNmcbiR2241oELjT7LQ9M3Yk",
-                name: "Avaulto faucet NFT",
-                sellerFeeBasisPoints: 500, // Represents 5.00%.
-            })
-        console.log(nft)
-        setAlertConfig({ open: true, message: 'Airdrop NFT to account' });
+            // init metaplex instance
+            const mx = new Metaplex(connection);
+            mx.use(keypairIdentity(feePayer));
+            const { nft } = await mx
+                .nfts()
+                .create({
+                    tokenOwner: new PublicKey(walletAddress),
+                    isCollection: true,
+                    uri: "https://yyuf64d3dxl7pzwpyvwb24vqgztrxci5w3rvubbogt5s2d2m3weq.arweave.net/xihfcHsd1_fmz8VsHXKwNmcbiR2241oELjT7LQ9M3Yk",
+                    name: "Avaulto faucet NFT",
+                    sellerFeeBasisPoints: 500, // Represents 5.00%.
+                })
+            console.log(nft)
+            setAlertConfig({ open: true, message: 'Airdrop NFT to account' });
+            setLoader(false)
+        } catch (error) {
+            setLoader(false);
+            console.error(error)
+        }
+
     }
 
-    const mintSPL = async () => {
-        trackEvent('airdrop spl') 
+    const mint = async (feePayer: Keypair, toWallet: PublicKey) => {
 
-        // create middleware account
-        const feePayer = createDemoAccount();
-
-        // airdrop this account
-        await requestAirdrop(feePayer.publicKey.toBase58())
-
-        const mint = Keypair.generate();
+        // create a mint account
+        let mint = Keypair.generate();
         console.log(`mint: ${mint.publicKey.toBase58()}`);
-        const { lastValidBlockHeight, blockhash } = await connection.getLatestBlockhash();
-        const txArgs: TransactionBlockhashCtor = { feePayer: feePayer.publicKey, blockhash, lastValidBlockHeight: lastValidBlockHeight }
 
-        // 1) use build-in function
-        let mintPubkey = await createMint(
-            connection, // conneciton
-            feePayer, // fee payer
-            new PublicKey(walletAddress), // mint authority
-            new PublicKey(walletAddress), // freeze authority (you can use `null` to disable it. when you disable it, you can't turn it on again)
-            8 // decimals
+        let tx = new Transaction();
+        tx.add(
+            // create account
+            SystemProgram.createAccount({
+                fromPubkey: feePayer.publicKey,
+                newAccountPubkey: mint.publicKey,
+                space: MINT_SIZE,
+                lamports: await getMinimumBalanceForRentExemptMint(connection),
+                programId: TOKEN_PROGRAM_ID,
+            }),
+            // init mint
+            createInitializeMintInstruction(
+                mint.publicKey, // mint pubkey
+                0, // decimals
+                toWallet, // mint authority (an auth to mint token)
+                null // freeze authority (we use null first, the auth can let you freeze user's token account)
+            )
         );
-        console.log(`mint: ${mintPubkey.toBase58()}`);
 
-        setAlertConfig({ open: true, message: 'Airdrop SPL to account' });
+        return { tx, mint };
+    }
+    const mintSPL = async () => {
+        try {
+
+            setLoader(true)
+            trackEvent('airdrop spl')
+            const feePayer = createDemoAccount()
+
+            // G2FAbFQPFa5qKXCetoFZQEvF9BVvCKbvUZvodpVidnoY
+            const toWallet = new PublicKey(walletAddress)
+            const mintIns = await mint(feePayer, toWallet);
+            const mintPubkey = new PublicKey("AjMpnWhqrbFPJTQps4wEPNnGuQPMKUcfqHUqAeEf1WM4");
+
+
+
+
+            let tokenAccount = Keypair.generate();
+            console.log(`ramdom token address: ${tokenAccount.publicKey.toBase58()}`);
+
+            const { lastValidBlockHeight, blockhash } = await connection.getLatestBlockhash();
+            const txArgs: TransactionBlockhashCtor = { feePayer: feePayer.publicKey, blockhash, lastValidBlockHeight: lastValidBlockHeight }
+
+
+
+
+
+            // create account
+            const ataIns = SystemProgram.createAccount({
+                fromPubkey: feePayer.publicKey,
+                newAccountPubkey: tokenAccount.publicKey,
+                space: ACCOUNT_SIZE,
+                lamports: await getMinimumBalanceForRentExemptAccount(connection),
+                programId: TOKEN_PROGRAM_ID,
+            })
+            // init token account
+            const initAtaIns = createInitializeAccountInstruction(tokenAccount.publicKey, mintPubkey, feePayer.publicKey)
+
+
+
+            let ata = await getAssociatedTokenAddress(
+                mintPubkey, // mint
+                feePayer.publicKey, // owner
+                false // allow owner off curve
+            );
+            console.log(`ata: ${ata.toBase58()}`);
+
+            const cata =
+                createAssociatedTokenAccountInstruction(
+                    feePayer.publicKey, // payer
+                    ata, // ata
+                    toWallet, // owner
+                    mintPubkey // mint
+
+                );
+
+            const ins = [mintIns.tx, ataIns, cata]
+            let transaction = new Transaction(txArgs).add(...ins);
+            const rawTransaction = transaction.serialize({ requireAllSignatures: false });
+            const signature = await connection.sendRawTransaction(rawTransaction);
+            await confirmAndFinalize(signature);
+            setAlertConfig({ open: true, message: 'Airdrop SPL to account' });
+            setLoader(false)
+        } catch (error) {
+            setLoader(false);
+            console.error(error)
+        }
     }
     return (
         <>
-            <Box style={{display:'flex', justifyContent:'space-between'}} sx={{ m:4, p: 2, border: '1px dashed grey' }}>
+            <Box style={{ display: 'flex', justifyContent: 'space-between' }} sx={{ m: 4, p: 2, border: '1px dashed grey' }}>
                 <Button onClick={handleAirdrop} disabled={!isValid} color={'success'} variant="contained">Get 1 SOL</Button>
                 <Button onClick={requestNft} disabled={!isValid} color={'secondary'} variant="contained">Get 1 NFT</Button>
-                <Button onClick={mintSPL} disabled={!isValid} color={'primary'} variant="contained">Mint & get SPL</Button>
+                {/* <Button onClick={mintSPL} disabled={!isValid} color={'primary'} variant="contained">Mint & get SPL</Button> */}
             </Box>
+            {loader && <CircularProgress color="secondary" style={{ display: 'flex', margin: '0 auto' }} />}
 
         </>
     )
